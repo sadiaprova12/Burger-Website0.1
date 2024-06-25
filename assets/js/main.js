ab6cc37df33e26574abc05ea14aa700d964414c2
sadiaprova12 <sadiaprova51717@gmail.com>
2024-06-25 21:44:49 +0600

    styled all souces

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,38 +34,38 @@ const shadowHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('shadow-header') 
                        : header.classList.remove('shadow-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', shadowHeader)
 
-/*=============== TESTIMONIAL SWIPER ===============*/
-let testimonialSwiper = new Swiper(".testimonial-swiper", {
-    spaceBetween: 30,
-    loop: 'true',
+// /*=============== TESTIMONIAL SWIPER ===============*/
+// let testimonialSwiper = new Swiper(".testimonial-swiper", {
+//     spaceBetween: 30,
+//     loop: 'true',
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
 
-/*=============== NEW SWIPER ===============*/
-let newSwiper = new Swiper(".new-swiper", {
-    spaceBetween: 24,
-    loop: 'true',
+// /*=============== NEW SWIPER ===============*/
+// let newSwiper = new Swiper(".new-swiper", {
+//     spaceBetween: 24,
+//     loop: 'true',
 
-    breakpoints: {
-        576: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
-    },
-});
+//     breakpoints: {
+//         576: {
+//           slidesPerView: 2,
+//         },
+//         768: {
+//           slidesPerView: 3,
+//         },
+//         1024: {
+//           slidesPerView: 4,
+//         },
+//     },
+// });
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
     
 const scrollActive = () =>{
@@ -95,53 +95,72 @@ const scrollUp = () =>{
 }
 window.addEventListener('scroll', scrollUp)
 
-/*=============== SHOW CART ===============*/
-const cart = document.getElementById('cart'),
-      cartShop = document.getElementById('cart-shop'),
-      cartClose = document.getElementById('cart-close')
 
-/*===== CART SHOW =====*/
-/* Validate if constant exists */
-if(cartShop){
-    cartShop.addEventListener('click', () =>{
-        cart.classList.add('show-cart')
-    })
-}
+/*=============== SCROLL REVEAL ANIMATION ===============*/
 
-/*===== CART HIDDEN =====*/
-/* Validate if constant exists */
-if(cartClose){
-    cartClose.addEventListener('click', () =>{
-        cart.classList.remove('show-cart')
-    })
-}
-
-/*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    // reset: true, // AMimations repeat
 })
+
+sr.reveal('.home__data, .footer')
+sr.reveal('.home__dish', {delay: 500, distance: '100px', origin: 'bottom'})
+sr.reveal('.home__burger', {delay: 1200, distance: '100px', duration: 1500})
+sr.reveal('.home__ingredient', {delay: 1600, interval: 100})
+sr.reveal('.recipe__img, .delivery__img, .contact__image',  {origin: 'left'})
+sr.reveal('.recipe__data, .delivery__data, .contact__data', {origin: 'right'})
+sr.reveal('.popular__card', {interval: 100})
+
+// /*=============== SHOW CART ===============*/
+// const cart = document.getElementById('cart'),
+//       cartShop = document.getElementById('cart-shop'),
+//       cartClose = document.getElementById('cart-close')
+
+// /*===== CART SHOW =====*/
+// /* Validate if constant exists */
+// if(cartShop){
+//     cartShop.addEventListener('click', () =>{
+//         cart.classList.add('show-cart')
+//     })
+// }
+
+// /*===== CART HIDDEN =====*/
+// /* Validate if constant exists */
+// if(cartClose){
+//     cartClose.addEventListener('click', () =>{
+//         cart.classList.remove('show-cart')
+//     })
+// }
+
+// /*=============== DARK LIGHT THEME ===============*/ 
+// const themeButton = document.getElementById('theme-button')
+// const darkTheme = 'dark-theme'
+// const iconTheme = 'bx-sun'
+
+// // Previously selected topic (if user selected)
+// const selectedTheme = localStorage.getItem('selected-theme')
+// const selectedIcon = localStorage.getItem('selected-icon')
+
+// // We obtain the current theme that the interface has by validating the dark-theme class
+// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// // We validate if the user previously chose a topic
+// if (selectedTheme) {
+//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+//   themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+// }
+
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener('click', () => {
+//     // Add or remove the dark / icon theme
+//     document.body.classList.toggle(darkTheme)
+//     themeButton.classList.toggle(iconTheme)
+//     // We save the theme and the current icon that the user chose
+//     localStorage.setItem('selected-theme', getCurrentTheme())
+//     localStorage.setItem('selected-icon', getCurrentIcon())
+// })
